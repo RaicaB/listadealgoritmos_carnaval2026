a3a1ff3c89db712361ce1283875f8c77c84241b4
RaicaB <raicamartins97@gmail.com>
2026-02-19 22:59:35 -0300

    Add files via upload

---
+++ atividade_4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double S = 0;
+        for (int i = 1; i <= 50; i++)
+        {
+            double numerador = (2 * i) - 1;
+            double denominador = i;
+            S += numerador / denominador;
+        }
+        Console.WriteLine($"O valor de S é: {S:F4}");
+    }
+}
