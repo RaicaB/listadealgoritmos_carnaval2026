7db346765258620327a8789f7d535b8ed25da732
RaicaB <raicamartins97@gmail.com>
2026-02-19 22:56:51 -0300

    Add files via upload

---
+++ atividade_3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a altura (em metros): ");
+        double altura = double.Parse(Console.ReadLine());
+        Console.Write("Digite o sexo (M para masculino, F para feminino): ");
+        string sexo = Console.ReadLine().ToUpper();
+        double pesoIdeal;
+        if (sexo == "M")
+        {
+            pesoIdeal = (72.7 * altura) - 58;
+else if (sexo == "F")
+}
+        {
+            pesoIdeal = (62.1 * altura) - 44.7;
+        }
+else
+        {
+            Console.WriteLine("Sexo inválido! Digite M ou F.");
+            return;
+        }
+        Console.WriteLine($"O peso ideal é: {pesoIdeal:F2} kg");
+    }
+}
