a9bd9ad16cc49418bbf5007ae493a1b174de5d74
RaicaB <raicamartins97@gmail.com>
2026-02-19 23:02:04 -0300

    Add files via upload

---
+++ atividade_5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o número de horas trabalhadas no mês: ");
+        int horasTrabalhadas = int.Parse(Console.ReadLine());
+        salarioTotal = horasTrabalhadas * valorHora;
+    }
+else
+{
+int horasExtras = horasTrabalhadas - horasNormaisMes;
+    double valorHoraExtra = valorHora * 1.5;
+    salarioTotal = (horasNormaisMes* valorHora) + (horasExtras* valorHoraExtra);
+}
+Console.WriteLine($"O salário total do funcionário é: R$ {salarioTotal:F2}");
+}
+}
