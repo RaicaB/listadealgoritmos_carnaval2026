4cb4522ddeb349ff2dea7a51a69dd195b40acb11
RaicaB <raicamartins97@gmail.com>
2026-02-19 22:52:53 -0300

    Add files via upload

---
+++ ex2_robinson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de A: ");
+        int A = int.Parse(Console.ReadLine());
+        Console.Write("Digite o valor de B: ");
+        int B = int.Parse(Console.ReadLine());
+        Console.Write("Digite o valor de C: ");
+        int C = int.Parse(Console.ReadLine());
+        int R = (A + B) * (A + B);
+        int S = (B + C) * (B + C);
+        double D = (R + S) / 2.0;
+        Console.WriteLine($"O valor de D é: {D}");
+    }
+}
